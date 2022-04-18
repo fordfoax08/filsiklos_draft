26b1764574ef43342be91156e5a5c692e925e0fd
Joms <jadizon.2325@gmail.com>
2022-04-18 13:32:23 +0800

    Update

--- src/components/home/ProductCard.js
+++ src/components/home/ProductCard.js
@@ -1,16 +1,16 @@
 const ProductCard = ({item}) => {
-    console.log(item.name)
+    // console.log(item.file)
     return(
         <div className="product-card">
             <div className="product-image">
                 <span className="discount-tag">50% off</span>
-                <img src={ `../../media/images/asdfsdf` } className="product-thumb" alt=""/>
+                <img src={ process.env.PUBLIC_URL+`/images/${ item.file }` } className="product-thumb" alt=""/>
                 <button className="card-btn">View details</button>
             </div>
             <div className="product-info">
                 <h2 className="product-brand">{ item.name }</h2>
-                <p className="product-short-description">a short line about the cloth..</p>
-                <span className="price">$20</span><span className="actual-price">$40</span>
+                <p className="product-short-description">{ item.shortDesc }</p>
+                <span className="price">$20</span><span className="actual-price"></span>
             </div>
         </div>
     )
