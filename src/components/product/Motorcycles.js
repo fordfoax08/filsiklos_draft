0d75bf4cf80d9dda3d943e44add9324c733ad45c
Joms <jadizon.2325@gmail.com>
2022-04-27 16:54:56 +0800

    Update

--- src/components/product/Motorcycles.js
+++ src/components/product/Motorcycles.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Motorcycles.css';
+import { Link } from 'react-router-dom';
+import ShopMenu from './ShopMenu';
 
 const Motorcycles = () => {
+    const [shopMenu, setShopMenu] = useState(false);
 
     return(
         <main className="main-motorcycles">
-            <h1>Motorcycles</h1>
+            <div className='shop-banner'>
+                <h3><Link to="/">HOME</Link> » MOTORCYCLES</h3>
+            </div>
+            <div className={`shop-menu ${ shopMenu && "open" }`}>
+                { shopMenu && <ShopMenu /> }
+                <button className={ `shop-menu-btn ${ shopMenu && "focus" }` } onClick={ () => setShopMenu(prev => !prev) }>»</button>
+            </div>
+            <div className='shop-container'></div>
         </main>
     )
 }
---
+++ src/components/product/ShopMenu.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './ShopMenu.css';
+
+function ShopMenu() {
+  return (
+    <form className='form-shop' onSubmit={ e => e.preventDefault() }>
+        <label htmlFor='search-shop'>Search:</label>
+        <input type="text" id="search" className='search-shop' name='search' placeholder='Search'/>
+        <div className='sort-container'>
+            <p>Filter</p>
+            <label htmlFor='sort-shop'></label>
+            <select id="sort-shop">
+                <option value="">Sort by</option>
+                <option value="price">Price</option>
+                <option value="name">Name</option>
+            </select>
+            <label htmlFor='order-shop'></label>
+            <select id="order-shop">
+                <option value="descend">Order by</option>
+                <option value="ascend">Ascend</option>
+                <option value="descend">Descend</option>
+            </select>
+        </div>
+        <label htmlFor='type-shop'>Motorcycle type:</label>
+        <select id="type-shop">
+            <option value="">All</option>
+            <option value="scooter">Scooter</option>
+            <option value="underbone">Underbone</option>
+            <option value="bigbike">Bikbike</option>
+            <option value="offroad">Offroad</option>
+        </select>
+        <label htmlFor='transmission-shop'>Transmission type:</label>
+        <select id="transmission-shop">
+            <option value="">All</option>
+            <option value="auto">Automatic</option>
+            <option value="manual">Manual</option>
+            <option value="semi">Semi</option>
+        </select>
+        <label htmlFor='brand-shop'>Brand:</label>
+        <select id="brand-shop">
+            <option value="">All</option>
+            <option value="honda">Honda</option>
+            <option value="yamaha">Yamaha</option>
+            <option value="suzuki">Suzuki</option>
+            <option value="kawasaki">Kawasaki</option>
+            <option value="ktm">KTM</option>
+            <option value="kymco">KYMCO</option>
+            <option value="rusi">RUSI</option>
+        </select>
+
+        <button className='shop-submit'>Search</button>
+    </form>
+  )
+}
+
+export default ShopMenu
