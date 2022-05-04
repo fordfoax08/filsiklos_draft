31dc26c7b9e7b0bdf1064036aad20e0351fd2e7a
Joms <jadizon.2325@gmail.com>
2022-05-04 22:08:00 +0800

    Update

--- src/components/product/CardSuggest.js
+++ src/components/product/CardSuggest.js
@@ -4,7 +4,13 @@ import './CardSuggest.css';
 function CardSuggest() {
   return (
     <div className='card-suggest-container'>
-        
+        <div className='card-suggest-image-container'>
+          <img src="/images/cbr150 s.JPG" alt="suggested" />
+        </div>
+        <div className='card-suggest-details-container'>
+          <h3>SOMETHING</h3>
+          <p>This is an example of suggested items listed below shop, you can see this when hovering your cursor into the items</p>
+        </div>
     </div>
   )
 }
